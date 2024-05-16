b089c1068eac7ae3ceb49a249496facd12ea73a5
cameronotoole44 <cameronotoole404@gmail.com>
2024-05-16 17:30:45 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import ColorBlock from './components/ColorBlock';
+import ColorForm from './components/ColorForm';
 
 function App() {
+  let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']);
+
+  let colorMap = colors.map((color, i) => {
+    return <ColorBlock color={color} key={i} />
+  })
+
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+
   return (
     <div className="App">
-
+      {colorMap}
+      <ColorForm addColor={addColor} />
     </div>
   );
 }
